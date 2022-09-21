9613b89593ccffb2da6f01b36e83c28158f3d075
Olena Zhuk <olenka1303@gmail.com>
2022-09-21 13:44:21 +0300

    add lesson 05

---
+++ Lesson-05.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+
+</head>
+<body>
+
+<form action="Lesson-05.php" method="POST" enctype="multipart/form-data">
+    <input type="text" name="name" placeholder="your name">
+    <input type="password" name="password" placeholder="parol"><br>
+    <h2>чоловічий</h2>
+    <input type="radio" name="sex" value="f" >
+    <h2>жіночий</h2>
+    <input type="radio" name="sex" value="m" >
+    <p>
+        Виберіть свій варіант
+    </p>
+    <h3>Варіант 1</h3>
+    <input type="checkbox"  value="a">
+    <h3>Варіант 2</h3>
+    <input type="checkbox" value="b">
+    <h3>Варіант 3</h3>
+    <input type="checkbox" value="c">
+    <input type="submit">
+
+</form>
+</body>
+</html>
+
+<?php
+
+if (isset($_POST['name'])) {
+    $ooo = $_POST['name'];
+    print 'Name: '.$ooo.'<br>';
+}
+
+if (isset($_POST['password'])) {
+    $pass = $_POST['password'];
+    print 'Pass: '.$pass.'<br>';
+}
